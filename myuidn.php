86dfc0ead80500d895fd6bc3957497d3fdbaad8b
ManuelSoftwareDev <mail@manuelsoftware.de>
2016-05-14 23:20:34 +0200

    Update
    - new ID, characters and numerics
    - blocked some pages(pornsites, urlshrinker, javascript:...)

--- myuidn.php
+++ myuidn.php
@@ -3,7 +3,7 @@
 <link rel="Stylesheet" type="text/css" href="css-box.css">
 <body>
 		<div class="sbox center uidn">
-		<h3> Meine UIDN bei OShort ist: <a href="http://s.manuelsoftware.de/statistikview?uidn=<? require_once "func.php"; echo getUIDN(); ?>"><font color="yellow">	<? require_once "func.php"; echo getUIDN(); ?> </font></a></h3>
+		<h3> Meine UIDN bei OShort ist: <a href="http://<? require_once "core/config.core.php"; print SHORT_DOMAIN; ?>/statistikview?uidn=<? require_once "func.php"; echo getUIDN(); ?>"><font color="yellow">	<? require_once "func.php"; echo getUIDN(); ?> </font></a></h3>
         <p>Diese funktioniert nur, wenn du schon einen Kurzlink angelegt hast!</p>
 <style>
 			.foot { color:grey; }
--- short.php
+++ short.php
@@ -9,9 +9,27 @@ if (banlist($_SERVER["REMOTE_ADDR"])) {
 }else {
 $link = $_SESSION["link"];
 $lnk = SHORT_DOMAIN;
-if(strpos($link,'link?k') || strpos($link,'r?i') || strpos($link,$lnk)) {
+if(strpos($link,'link?k') || strpos($link,'r?i') || strpos($link,str_replace("https://", "", str_replace("http://","",$lnk)))) {
 	echo "<div class=\"alert error\">";
 	echo '<span>FEHLER:</span> Du kannst keinen Kurzlink k&uuml;rzen!';
+	echo "</div>";
+	return false;
+}
+if(strpos($link,'porn') || strpos($link,'sex') || strpos($link,'fick') || strpos($link,'fapdu') || strpos($link,'fabdu') || strpos($link,'xxx') || strpos($link,'bumsen') || strpos($link,'voegeln') || strpos($link,'xhamster') || strpos($link,'bitch') || strpos($link,'geilemaedchen') || strpos($link,'penis') || strpos($link,'vagina') || strpos($link,'muschi') || strpos($link,'lexyroxx') || strpos($link,'gay') || strpos($link,'123video') || strpos($link,'onlydudes') || strpos($link,'429tube') || strpos($link,'menhub') || strpos($link,'menhub') || strpos($link,'fotze')) {
+	echo "<div class=\"alert error\">";
+	echo '<span>FEHLER:</span> Diese Seiten werden nicht gek&uuml;rzt. Bitte beachte das Pornoseiten oder Seiten mit Illegalen Inhalten aus unserer Datenbank sofort entfernt werden!';
+	echo "</div>";
+	return false;
+}
+if(strpos($link,'bit.ly') || strpos($link,'adf.ly') || strpos($link,'short.tk') || strpos($link,'goo.gl') || strpos($link,'tinyurl.com') || strpos($link,'ow.ly') || strpos($link,'su.pr') || strpos($link,'is.gd') || strpos($link,'short.nr') || strpos($link,'youtu.be') || strpos($link,'para.pt')) {
+	echo "<div class=\"alert error\">";
+	echo '<span>FEHLER:</span> Du kannst keine Links von anderen URL Shrinkern k&uuml;rzen!';
+	echo "</div>";
+	return false;
+}
+if(strpos($link,'javascript:') || strpos($link,'whatsapp:')) {
+		echo "<div class=\"alert error\">";
+	echo '<span>FEHLER:</span> Ung&uuml;ltiges Protokoll vor dem URL!';
 	echo "</div>";
 	return false;
 }
@@ -19,7 +37,21 @@ if (!(isset($link))) {
     echo "<p>Fehler beim K&uuml;rzen. Kein Link angegeben!</p>";
 	return false;
 }
-$klink = rand(10000,99999);
+neu: $klink = "";
+$chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+for($i = 1; $i <= 6; $i++)
+{
+  $i2 = rand(0,1);
+  switch($i2)
+  {
+	  case 0:
+	   $klink = $klink . rand(0,9);
+	  break;
+	  case 1:
+	   $klink = $klink . $chars[rand(0,52)];
+	  break;
+  }  
+}
 $uidn = getUIDN();
 $table = DB_TABLE;
 $query = "INSERT INTO `$table` (`ip`, `to`, `link`, `uidn`, `visits`, `create`) VALUES('".$_SERVER["REMOTE_ADDR"]."', '".$mysqli->real_escape_string($link)."', '".$mysqli->real_escape_string($klink)."', '".$mysqli->real_escape_string($uidn)."', '0', '". Date("Y-m-d") . "');";
